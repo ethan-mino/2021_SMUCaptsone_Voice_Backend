0579f89fc0feb36161a96cd59433bce7d8bcb897
heejin99 <wkdgmlwls30@naver.com>
2021-09-05 22:59:06 +0900

    DB Gitignore 및 수정 - 희진

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,14 @@
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
-    // aws 용 - 톡방에 비밀번호와 user 남기겠습니다.
-    // host: 'urvoice.cir16abmh0e7.us-east-2.rds.amazonaws.com',
-    // user: '',
-    // password: '',
-    // port: ,
-    // database: 'urvoice'
-
-    // local test 용 - heejin
-    host: 'localhost',
-    user: 'root',
+    // aws
+    host: '',
+    user: '',
     password: '',
-    port: 3306,
-    database: 'urvoice'
+    port: 0,
+    database: ''
 })
 
 db.connect();
-
-// MySQL Terminal -> DB 생성 create table
-// CREATE TABLE urvoice (
-//     id varchar(50) NOT NULL,
-//     password varchar(100) NOT NULL,
-//     PRIMARY KEY (id)
-// );
 
 module.exports = db;
